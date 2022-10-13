2bda24deb9bd8f8ec892e96c7e57d3a37bfb5534
jkkang <jkkang@smartauth.kr>
2022-10-13 13:45:52 +0900

    update offset value
    - end value is exclusive in contract

--- config/votera.js
+++ config/votera.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
     proposalInfoHost: env('PROPOSAL_INFO_HOST'),
     services: {
         assess_begin_offset: env.int('ASSESS_BEGIN_OFFSET', 0),
-        assess_end_offset: env.int('ASSESS_END_OFFSET', 86399),
+        assess_end_offset: env.int('ASSESS_END_OFFSET', 86400),
         vote_begin_offset: env.int('VOTE_BEGIN_OFFSET', 0),
-        vote_end_offset: env.int('VOTE_END_OFFSET', 86399),
+        vote_end_offset: env.int('VOTE_END_OFFSET', 86400),
         vote_open: env.int('VOTE_OPEN_TIME', 30),
         can_withdraw_after: env.int('WITHDRAW_AFTER', 86400),
     },
