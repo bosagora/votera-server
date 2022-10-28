fa5b1979c0f37c54bcf2aed7870c2ae715c0d9fa
jkkang <jkkang@smartauth.kr>
2022-10-28 14:52:56 +0900

    투표 종료 후 나의 투표 결과 보여주기 기능 추가

--- api/ballot/controllers/ballot.js
+++ api/ballot/controllers/ballot.js
@@ -1,5 +1,6 @@
 'use strict';
 const { sanitizeEntity } = require('strapi-utils');
+const { ENUM_PROPOSAL_STATUS_CLOSED } = require('../../../src/types/proposal');
 const { ENUM_CANDIDATE_MAX } = require('../../../src/types/VoteraVoteType');
 
 module.exports = {
@@ -22,5 +23,17 @@ module.exports = {
 
         const ballot = await strapi.services.ballot.recordBallot(proposalId, address, commitment, transactionHash);
         return sanitizeEntity(ballot, { model: strapi.models.ballot });
+    },
+    async listMyBallots(ctx) {
+        const { _proposalId, _actor, _start, _limit, _sort } = ctx.params;
+        if (!_proposalId || !_actor) return ctx.badRequest('missing parameters');
+        await strapi.services.member.authorizeMember(_actor, ctx.state.user);
+
+        const proposal = await strapi.query('proposal').findOne({ proposalId: _proposalId });
+        if (!proposal) return ctx.notFound('notFound proposal');
+        if (proposal.status !== ENUM_PROPOSAL_STATUS_CLOSED) return ctx.badRequest('NotClosed proposal');
+
+        const response = await strapi.services.ballot.listMyBallots(proposal, _actor, _start, _limit, _sort);
+        return response;
     },
 };
